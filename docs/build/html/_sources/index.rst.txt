252fe73d2a6815ce965550a9994575241c1ad25b
Juan José Quiroz Omaña <juanjqogm@gmail.com>
2023-07-20 13:08:49 +0900

    u

---
+++ docs/build/html/_sources/index.rst.txt
@@ -0,0 +1,16 @@
+Welcome to Test documentation!
+==============================
+
+
+.. |logo| image:: ../images/logo.svg
+.. |logopng| image:: ../images/logo.png
+.. |dqroboticslogo| image:: ../images/dqrobotics_logo_infinity.svg
+.. |dqroboticslogo2| image:: ../images/dqrobotics_logo_infinity2.svg
+
+
+.. image:: ../images/cover.png
+   :align: center
+
+.. note::
+
+   This project is under active development.
---
+++ docs/source/conf.py
@@ -0,0 +1,104 @@
+# Configuration file for the Sphinx documentation builder.
+
+import os
+import sys
+from pathlib import Path
+sys.path.append(str(Path(".").resolve()))
+# -- Project information
+
+path_str = str(Path(__file__).parent.parent)
+
+# -- Project information
+
+project = 'MyTutorial'
+copyright = '2023, Juancho'
+author = 'Juancho'
+
+release = '0.1'
+version = '0.1.0'
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+#html_theme = 'sphinx_rtd_theme'
+html_theme = "pydata_sphinx_theme"
+html_logo = path_str + "/build/html/_images/logo.svg"
+html_favicon = path_str + "/build/html/_images/logo.svg"
+
+
+html_theme_options = {
+    "external_links": [
+
+        {
+            "url": "https://github.com/dqrobotics/learning-dqrobotics-in-matlab",
+            "name": "Learn DQ Robotics",
+        },
+        {
+            "url": "https://hal.science/hal-01478225v1",
+            "name": "Learn Dual Quaternion Algebra",
+        },
+    ],
+    "header_links_before_dropdown": 4,
+    "icon_links": [
+
+        {
+            "name": "GitHub",
+            "url": "https://dqrobotics.github.io/",
+            "icon": "fa-brands fa-github",
+        },
+
+        {
+            "name": "DQ Robotics",
+            "url": "https://dqrobotics.github.io/",
+            "icon": path_str + "/build/html/_images/dqrobotics_logo_infinity2.svg",
+            "type": "local",
+            "attributes": {"target": "_blank"},
+        },
+    ],
+    # alternative way to set twitter and github header icons
+    # "github_url": "https://github.com/pydata/pydata-sphinx-theme",
+    # "twitter_url": "https://twitter.com/PyData",
+    "logo": {
+        "text": "Home",
+        "image_dark": "/docs/build/html/_images/logo.svg",
+        "alt_text": "Tutorial: DQ Robotics with CoppeliaSim",
+    },
+    "use_edit_page_button": False,
+    "show_toc_level": 1,
+    "navbar_align": "left",  # [left, content, right] For testing that the navbar items align properly
+    #"navbar_center": ["version-switcher", "navbar-nav"],
+    #"announcement": "https://raw.githubusercontent.com/pydata/pydata-sphinx-theme/main/docs/_templates/custom-template.html",
+    # "show_nav_level": 2,
+    # "navbar_start": ["navbar-logo"],
+    # "navbar_end": ["theme-switcher", "navbar-icon-links"],
+    # "navbar_persistent": ["search-button"],
+    # "primary_sidebar_end": ["custom-template.html", "sidebar-ethical-ads.html"],
+    # "article_footer_items": ["test.html", "test.html"],
+    # "content_footer_items": ["test.html", "test.html"],
+    # "footer_start": ["test.html", "test.html"],
+    # "secondary_sidebar_items": ["page-toc.html"],  # Remove the source buttons
+
+    #"primary_sidebar_end": ["sidebar-ethical-ads"],
+    # "search_bar_position": "navbar",  # TODO: Deprecated - remove in future version
+}
+
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
